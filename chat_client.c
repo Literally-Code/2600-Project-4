dcccc8b645e654c9ae3c6330ec1bf4edc1e31d95
Jack R. Woline <jonwoline@gmail.com>
2025-04-29 22:01:50 -0700

    added to chat_client.c

--- chat_client.c
+++ chat_client.c
@@ -1,4 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define HOST "localhost"
+#define PORT 59222
+
 int main()
 {
+	int client_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+	if (client_fc < 0)
+	{
+		perror("Could not open socket\n");
+		exit(-1);
+	}
+
+	struct hostend* host_ptr = gethostbyname(HOST);
+	
+	if (host_ptr == NULL)
+	{
+		perror("Could not get host\n");
+		exit(-1);
+	}
+
+	if (host_ptr->h_addrtype != AF_INET)
+	{
+		perror("Bad address family\n");
+		exit(-1);
+	}
+
+	struct sockaddr_in server_addr;
+	memset(&server_addr, 0, sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = ((struct in_addr*)hptr->h_addr_list[0])->s_addr;
+	server_addr.sin_port = htons(PORT);
+	
+	// TODO: Connection and transmission
+
 	return 0;
 }
